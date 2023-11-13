7d154418818abc08328db4c303a140e1a86d37c6
zeus-dev <62093083+zeus-dev@users.noreply.github.com>
2023-11-13 13:04:39 +0530

    Update gas_fee_split .sol
    initial pause

--- gas_fee_split .sol
+++ gas_fee_split .sol
@@ -9,10 +9,23 @@ contract CustomToken {
     address public owner;
     address public feeAddress; // Address to receive the flat fee
     mapping(address => uint256) public balanceOf;
+    bool public paused; // New state variable to track contract's paused status
 
     uint256 public flatRateFee = 24e15; // 0.024 tokens (assuming the token has 18 decimals)
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Pause();
+    event Unpause();
+
+    modifier whenNotPaused() {
+        require(!paused, "Contract is paused");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
 
     constructor(
         string memory _name,
@@ -28,9 +41,11 @@ contract CustomToken {
         feeAddress = _feeAddress; // Set the fee address
         balanceOf[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
+        paused = true; // Contract is paused initially
+        emit Pause();
     }
 
-    function transfer(address to, uint256 value) public returns (bool) {
+    function transfer(address to, uint256 value) public whenNotPaused returns (bool) {
         require(to != address(0), "Invalid address");
         require(balanceOf[msg.sender] >= value, "Insufficient balance");
 
@@ -46,8 +61,19 @@ contract CustomToken {
         return true;
     }
 
-    function setFlatRateFee(uint256 newFee) public {
-        require(msg.sender == owner, "Only the owner can set the flat rate fee");
+    function setFlatRateFee(uint256 newFee) public onlyOwner {
         flatRateFee = newFee;
+    }
+
+    function pause() public onlyOwner {
+        require(!paused, "Contract is already paused");
+        paused = true;
+        emit Pause();
+    }
+
+    function unpause() public onlyOwner {
+        require(paused, "Contract is not paused");
+        paused = false;
+        emit Unpause();
     }
 }
